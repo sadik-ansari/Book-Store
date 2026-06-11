5e930887947eedc037bc1283fb4229b4a35c2778
sadik ansari <mohammedsadikansari75@gmail.com>
2026-06-11 16:39:57 +0530

    changing the base url like localhost into onrender

--- client/src/components/api/BookApi.js
+++ client/src/components/api/BookApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `http://localhost:5000/api/books`;
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/books`;
 
 // Helper function for error handling
 const handleError = (error) => {
